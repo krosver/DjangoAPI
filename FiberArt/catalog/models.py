dd612672b654154ae2852926fbfb7b4c33a1102f
Krosver <kirill.kutyrev@gmail.com>
2024-01-07 17:49:11 +0300

    Create models

--- FiberArt/catalog/models.py
+++ FiberArt/catalog/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
-class MyModelName(models.Model):
-    my_field_name = models.CharField(max_length=20, help_text='Введите описание поля')
+class TypeEmbroidery(models.Model):
+    Name = models.CharField(max_length=20, help_text='Название типа вышивки')
 
     def __str__(self):
-        """Строка для представления объекта MyModelName (например, в административной панели и т.д.)."""
-        return self.my_field_name
+        return self.Name
+
+
+class Embroidery(models.Model):
+    User_id = models.ForeignKey(User, on_delete=models.CASCADE, help_text='Создатель')
+    Name = models.CharField(max_length=40, help_text='Название схемы')
+    Type_id = models.ForeignKey(TypeEmbroidery, on_delete=models.CASCADE, help_text='Тип вышивки')
+    SizeX = models.IntegerField(help_text='Количество клеток по горизонтали')
+    SizeY = models.IntegerField(help_text='Количество клеток по вертикали')
+    Scheme = models.TextField(help_text='Массив цветов')
+    Date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"User: {self.User_id} Name: {self.Name} Date: {self.Date}"
+
+
+class Color(models.Model):
+    Name = models.CharField(max_length=40, help_text='Название цвета')
+    HEX = models.CharField(max_length=7, help_text='HEX данного цвета')
+
+    def __str__(self):
+        return self.Name
